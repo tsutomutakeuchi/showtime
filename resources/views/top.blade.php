c30ec76b8ac2cd7876a422fd7b398e1d1ca0f234
tsutomu takeuchi <disneyland.dazo23@gmail.com>
2023-08-17 10:55:38 +0000

    レイアウト用の共通点プレートを作成 #9

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/common.blade.phpを読み込む --}}
+@extends('layouts.common')
 
-        <title>showtime</title>
-    </head>
-    <body>
-        <h1>shotime</h1>
-    </body>
-</html>
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'showtime')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>showtime</h2>
+            </div>
+        </div>
+    </div>
+@endsection
